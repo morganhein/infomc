c13143fb0d25206fe4b992b21d5356dfb6ded618
Morgan H <git@morganhein.com>
2013-10-05 09:44:27 -0700

    Fixed permissions, updated healthbar, and fixed craftbook.

--- startserver.sh
+++ startserver.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
  BINDIR=$(dirname "$(readlink -fn "$0")")
  cd "$BINDIR"
- java -Xmx4G -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -server -jar craftbukkit.jar -o true
+ java -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -server -jar craftbukkit.jar -o true
